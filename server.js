0094a0121454fd0a999d7a1c9d5a9f43f421d3ca
Carter Rabasa <carter@rabasa.com>
2012-07-01 12:08:05 -0700

    added JSONP support

--- server.js
+++ server.js
@@ -9,9 +9,15 @@ var server = http.createServer(function (request, response) {
   var u = url.parse(request.url, true);
   if (u.pathname === '/transcode' && u.query.str) {
     response.writeHead(200, {"Content-Type": "application/json;charset=utf-8"});
-    var str = url.parse(request.url, true).query.str;
+    var str = u.query.str;
     var result = {input: str, output: sym.transcode(str) };
-    response.end(JSON.stringify(result));
+    if (u.query.callback) {
+      response.end(u.query.callback+'('+JSON.stringify(result)+');');
+    }
+    else {
+      response.end(JSON.stringify(result));
+    }
+
   }
   else if (u.pathname === '/sms') {
     response.writeHead(200, {"Content-Type": "text/xml;charset=utf-8"});
